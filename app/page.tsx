b2add1721398cce0e56f1bb31bea21c676f46156
Gianluca Donato <gian.dnt@gmail.com>
2023-12-06 00:23:06 +0100

    add login form

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,43 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import gspaceLogo from "../public/gspace.svg";
+import gspaceLogo2 from "../public/gspace-logo.svg";
 
 import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 import { Button } from "@/components/ui/button";
+import { LoginForm } from "@/components/login-form";
 
 export default function Home() {
   return (
     <main className="h-screen w-screen bg-stone-950">
       <div className="bg-radial h-full w-full"></div>
       <div className="fixed top-0 flex h-screen w-screen items-center justify-center">
-        <Image width="400" src={gspaceLogo} alt="gspace logo"></Image>
+        <Image width="400" src={gspaceLogo} alt="gspace logo" />
       </div>
+      
       <div className="fixed top-0 flex w-screen justify-end p-4">
-        <AlertDialog>
-          <AlertDialogTrigger asChild>
-            <Button>Login</Button>            
-          </AlertDialogTrigger>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction>Continue</AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline">Login</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <div className="w-[250px] mx-auto">
+                <Image width="400" src={gspaceLogo2} alt="gspace logo" />
+              </div>
+            </DialogHeader>
+            <div className="p-4 pb-2">
+              <LoginForm />
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
     </main>
   );
---
+++ components/login-form.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z.string().min(1, {
+    message: "Insert username",
+  }),
+  password: z.string().min(1, {
+    message: "Insert password",
+  }),
+});
+
+export function LoginForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log("onSubmit", values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" type="submit">
+          Sign in
+        </Button>
+      </form>
+    </Form>
+  );
+}
